061d4df344e241f3b65adeaa810ef09f865c0dfe
AudinoPantoja <pantojabel@gmail.com>
2024-11-05 23:18:08 -0500

    Modified Sever Express

--- build/index.js
+++ build/index.js
@@ -6,18 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/number/:name', (req, res) => {
+app.get('/', (_req, res) => {
+    res.send('¡Hola, Bienvenido al Mundo de express!');
+});
+app.get('/parameter/:name', (req, res) => {
     const { name } = req.params;
-    const lastname = req.query.lastname ? req.query.lastname : 'Apellido no especificado';
-    res.send(`Hola, ${name}! ${lastname ? `apellido: ${lastname}.` : ''}`);
+    const lastname = req.query.lastname || 'Apellido no especificado';
+    const ubication = req.query.ubication || 'Ubicación no especificada';
+    res.send(`Hola, ${name}! ${lastname ? `Eres de apellido: ${lastname},` : ''} ${ubication ? `y vives en: ${ubication}.` : ''}`);
 });
 app.get('/info', (req, res) => {
     const name = req.query.name ? req.query.name : 'Desconocido';
     const age = req.query.age ? req.query.age : 'Edad no especificada';
     res.send(`Hola ${name}, tienes ${age} años`);
-});
-app.get('/hello', (_req, res) => {
-    res.send('¡Hola, Mundo!');
 });
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-// Define el puerto
 const port = 3000;
 
-// Define una ruta con un parámetro en la URL y consulta opcional
-app.get('/number/:name', (req: Request, res: Response) => {
-    const { name } = req.params; // Obtiene el parámetro de la URL
-    const lastname = req.query.lastname ? req.query.lastname : 'Apellido no especificado'; // Obtiene el parámetro de consulta
-
-    res.send(`Hola, ${name}! ${lastname ? `apellido: ${lastname}.` : ''}`);
+// Otra ruta de ejemplo sin parámetros
+app.get('/', (_req: Request, res: Response) => {
+    res.send('¡Hola, Bienvenido al Mundo de express!');
 });
+
+// Define una ruta con un parámetro en la URL y consulta opcional
+app.get('/parameter/:name', (req: Request, res: Response) => {
+    const { name } = req.params; // Obtiene el parámetro de la URL
+    const lastname = req.query.lastname || 'Apellido no especificado'; // Obtiene el parámetro de consulta "lastname"
+    const ubication = req.query.ubication || 'Ubicación no especificada'; // Obtiene el parámetro de consulta "ubication"
+
+    res.send(`Hola, ${name}! ${lastname ? `Eres de apellido: ${lastname},` : ''} ${ubication ? `y vives en: ${ubication}.` : ''}`);
+});
+
 
 // Ruta que maneja parámetros de consulta (query) solamente
 app.get('/info', (req: Request, res: Response) => {
@@ -20,13 +26,8 @@ app.get('/info', (req: Request, res: Response) => {
     res.send(`Hola ${name}, tienes ${age} años`);
 });
 
-// Otra ruta de ejemplo sin parámetros
-app.get('/hello', (_req: Request, res: Response) => {
-    res.send('¡Hola, Mundo!');
-});
-
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
-    // colocar http://localhost:3000/number/Audino?lastname=Pantoja
+    // colocar http://localhost:3000/parameter/Audino?lastname=Pantoja&ubication=Mocoa
     // http://localhost:3000/info?name=Audino&age=34
 });
